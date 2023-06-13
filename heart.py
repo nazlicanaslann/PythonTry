7a46e92940df0fe93a81a52680ad86f89b0599b3
Nazlıcan <101356820+nazlicanaslann@users.noreply.github.com>
2023-06-13 11:37:49 +0300

    Draw a Heart

---
+++ heart.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat July 14 20:46:46 2022
+
+@author: nazli
+"""
+
+
+from turtle import *
+bgcolor("black") #background color
+color("red") #pencolor
+begin_fill() 
+pensize(3) 
+left(50) 
+forward(133) 
+circle(50,200) #rightside
+right(140) 
+circle(50,200) #leftside
+forward(133) 
+end_fill() 
+input() #run screen stable
